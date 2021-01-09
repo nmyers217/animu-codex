91db95a032eb7b84c10fd29e5099dd929499b367
nmyers217 <nick.b.myers@protonmail.com>
2021-01-08 21:49:31 -0600

    Add tailwindcss to the project

--- src/pages/MediaListPage.tsx
+++ src/pages/MediaListPage.tsx
@@ -5,7 +5,7 @@ import MediaList from '../components/MediaList';
 function MediaListPage() {
   return (
     <>
-      <h2>Animu & Mango</h2>
+      <h2 className="text-xl font-medium text-black">Animu & Mango</h2>
       <MediaList />
     </>
   );
